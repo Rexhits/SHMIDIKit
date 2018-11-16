3e7aba64b0a7047dd03cd50731917097ebcdd11f
Rexhits <wangrex@Rexs-Mac.local>
2018-11-15 20:20:00 -0500

    11-15-2018

---
+++ SHMIDIKit_Mac/SHMIDIKit_Mac.h
@@ -0,0 +1,18 @@
+//
+//  SHMIDIKit_Mac.h
+//  SHMIDIKit_Mac
+//
+//  Created by WangRex on 11/15/18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SHMIDIKit_Mac.
+FOUNDATION_EXPORT double SHMIDIKit_MacVersionNumber;
+
+//! Project version string for SHMIDIKit_Mac.
+FOUNDATION_EXPORT const unsigned char SHMIDIKit_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHMIDIKit_Mac/PublicHeader.h>
+
+
